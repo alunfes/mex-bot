c00d80a380f4a3c4f366a59691171aa6495592e5
alunfes <ts4792@gmail.com>
2020-01-06 07:18:47 +0900

    6Jan

--- RealtimeSim.py
+++ RealtimeSim.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from RealtimeSimStrategy import RealtimeSimStrategy
 from RealtimeSimAccount import RealtimeSimAccount
 from OneMinData import OneMinData
+from RealtimeWSAPI import TickData
 import time
 
 
@@ -37,6 +38,29 @@ class RealtimeSim:
             ac.update_pnl(ltp)
             return ac
 
+    @classmethod
+    def sim_model_pred_onemin_limit_entry(cls, pred, pt_ratio, lc_ratio, amount, ltp, ac: RealtimeSimAccount, ohlc: OneMinData):
+        ds = RealtimeSimStrategy.model_prediction_onemin(pred, lc_ratio, amount, ac, ltp)
+        pt_price = int(round(ac.holding_price * (1.0 + pt_ratio))) if ac.holding_side == 'Buy' else int(round(ac.holding_price * (1.0 - pt_ratio)))
+        if ds.flg_noaction == False:
+            if ds.order_type == 'LC':  # losscut
+                ac.cancel_all_orders()
+                if ac.holding_side != '':  # do losscut
+                    ac.entry_order('Buy' if ac.holding_side == 'Sell' else 'Sell', 0, ac.holding_size, 'Market', ltp, datetime.now())
+            else:  # not losscut
+                if ds.posi_side != ac.holding_side and ds.posi_side != '':
+                    if ac.holding_side == '':
+                        ac.entry_order(ds.posi_side, TickData.get_bid() if ds.posi_side == 'Buy' else TickData.get_ask(), ds.posi_size, 'Limit', ltp, datetime.now())
+                    else:
+                        ac.cancel_all_orders()  # to cancel old pt order after changed position
+                        ac.entry_order(ds.posi_side, TickData.get_bid() if ds.posi_side == 'Buy' else TickData.get_ask(), ds.posi_size + ac.holding_size, 'Limit', ltp, datetime.now())
+                elif ds.order_type == 'PT' and (ds.order_side not in ac.order_side.values()):
+                    ac.entry_order('Buy' if ac.holding_side == 'Sell' else 'Sell', pt_price, ac.holding_size, 'Limit', ltp, datetime.now())
+                elif ds.posi_side == ac.holding_side and ds.posi_size != ac.holding_size:
+                    print('position size unmatched!')
+        ac.check_execution(ltp, datetime.now(), ohlc)
+        ac.update_pnl(ltp)
+        return ac
 
     @classmethod
     def sim_model_pred_onemin_avert(cls, pred, pt_ratio, lc_ratio, amount, ltp, ac:RealtimeSimAccount, ac2:RealtimeSimAccount, avert_period_kijun, avert_val_kijun, ohlc:OneMinData):
@@ -45,6 +69,27 @@ class RealtimeSim:
             if np.gradient(ta.MA(np.array(ac2.total_pl_log[-avert_period_kijun:], dtype='f8'), timeperiod=avert_period_kijun))[-1] > avert_val_kijun:
                 if ac2.holding_side != ac.holding_side and ac2.holding_side != '':
                     ac.entry_order(ac.holding_side, ltp-100, ac2.holding_size, 'Market')
+                    print('sim onemin avert: Market order for position  {', ac.holding_side, ' x ', ac.holding_size, ' @', ac.holding_price + '}')
+                elif ac2.order_side != ac.order_side:
+                    if ac.order_side != '':
+                        ac.cancel_all_orders()
+                    ac.entry_order(ac2.order_side, ac2.price, ac2.size, ac2.order_type, ltp, datetime.now())
+                    print('sim onemin avert:' + ac2.order_type + ' entry order {', ac2.orderside, ' x ', ac.holding_size, ' @', ac.holding_price + '}')
+        else:  # logがたまるまでは普通にトレード
+            ac = cls.sim_model_pred_onemin(pred, pt_ratio, lc_ratio, amount, ltp, ac, ohlc)
+        ac.check_execution(ltp, datetime.now(), ohlc)
+        ac.update_pnl(ltp)
+        ac2.check_execution(ltp, datetime.now(), ohlc)
+        ac2.update_pnl(ltp)
+        return ac
+
+    @classmethod
+    def sim_model_pred_onemin_avert_limit_entry(cls, pred, pt_ratio, lc_ratio, amount, ltp, ac:RealtimeSimAccount, ac2:RealtimeSimAccount, avert_period_kijun, avert_val_kijun, ohlc:OneMinData):
+        ac2 = cls.sim_model_pred_onemin(pred, pt_ratio, lc_ratio, amount, ltp, ac2, ohlc)
+        if len(ac2.total_pl_log) > avert_period_kijun:  # pl_check_term以上のpl logが溜まったらcheckを開始
+            if np.gradient(ta.MA(np.array(ac2.total_pl_log[-avert_period_kijun:], dtype='f8'), timeperiod=avert_period_kijun))[-1] > avert_val_kijun:
+                if ac2.holding_side != ac.holding_side and ac2.holding_side != '':
+                    ac.entry_order(ac2.holding_side, TickData.get_bid() if ac2.posi_side == 'Buy' else TickData.get_ask(), ac2.holding_size, 'Limit')
                     print('sim onemin avert: Market order for position  {', ac.holding_side, ' x ', ac.holding_size, ' @', ac.holding_price + '}')
                 elif ac2.order_side != ac.order_side:
                     if ac.order_side != '':
